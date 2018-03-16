3e912e03006e750495776a0316327af3b29ad158
rhit99900 <rohitcse081@gmail.com>
2018-03-16 18:33:21 +0530

    Text and UX Changes

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include ('db/config.php'); //Find the configuratio files in db/config.php
 
     <head>
         <meta charset="UTF-8">
-        <title>Fellowship Sign Up Form - Make A Difference</title>
+        <title>Fellowship Sign Up Form 2018 - Make A Difference</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
